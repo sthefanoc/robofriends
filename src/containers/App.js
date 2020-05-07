76ffbf485fcfd3b5456bc75011fffb2401db76f9
Sthefano Carvalho <sthefanocarvalho.consultoria@gmail.com>
2020-05-07 10:21:47 -0300

    ErrorBoundry, github pages and title changed

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
 // import { robots } from './robots';
 import './App.css';
 
@@ -39,7 +40,9 @@ class App extends Component {
                 <h1 className='f1'>RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange} />
                 <Scroll>
-                    <CardList robots ={filteredRobots} />
+                    <ErrorBoundry>
+                        <CardList robots ={filteredRobots} />
+                    </ErrorBoundry>
                 </Scroll>
             </div>
         )
